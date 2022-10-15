08cf07bf1df4b4328c9a2bb93fc7fb2c4a505919
alifsyahakbar <alifsyah789@gmail.com>
2022-10-15 18:39:04 +0700

    create liveness&readiness

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/health/live', function () {
+    return 'ok';
+});
+
+Route::get('/health/ready', function () {
+    $check = DB::connection()->getDatabaseName();
+    if ($check > 0) {
+        return 'ok';
+    }
+    return response('no', 500);
 });
